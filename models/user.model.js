651975833390b0f1e349a4a87391d5380839bed9
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-02-01 12:33:49 -0500

    updated User model

--- models/user.model.js
+++ models/user.model.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: [true, "Please add a name"] },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", userSchema);
-module.export = Book;
+module.export = User;
