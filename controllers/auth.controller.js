c125e9e995b212532a2f2e9f051874a790dcc120
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-02-01 13:37:50 -0500

    added register new user route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,47 @@
+import User from "../models/user.model.js"; // Ensure the correct file extension
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+  console.log(req.body);
+  const { firstName, lastName, email, password } = req.body;
+
+  console.log(firstName, lastName, email, password);
+
+  if (!firstName || !lastName || !email || !password) {
+    console.log("All fields are required");
+    return res.status(400).json({ message: "Please add all fields" });
+  }
+
+  try {
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log("hashedPassword", hashedPassword);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      res.status(201).json({
+        firstName,
+        lastName,
+        email,
+      });
+    }
+  } catch (error) {
+    console.error("Registration Error:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
 export const login = (req, res) => {
   console.log("login");
   res.send("login");
@@ -5,8 +49,4 @@ export const login = (req, res) => {
 
 export function logout(req, res) {
   res.send("Logout route");
-}
-
-export function register(req, res) {
-  console.log(req.body);
 }
--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
@@ -10,4 +10,4 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
-module.export = User;
+export default User;
