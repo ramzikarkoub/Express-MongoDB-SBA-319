a4ce6a9bcf5dbe84967e9b06e65219d76ab55c46
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-02-02 20:31:18 -0500

    fixed Comments Routes

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -2,7 +2,9 @@ import Comment from "../models/comment.model.js";
 
 export const createComment = async (req, res) => {
   const { content, postId } = req.body;
-  const author = req.user.userId;
+  console.log(req.body);
+  const author = req.user._id;
+  console.log(content, postId, author);
 
   try {
     const comment = await Comment.create({ content, author, post: postId });
@@ -14,11 +16,28 @@ export const createComment = async (req, res) => {
 
 export const getCommentsByPost = async (req, res) => {
   try {
-    const comments = await Comment.find({ post: req.params.postId }).populate(
-      "author",
-      "firstName lastName"
-    );
+    console.log(req.params.postId);
+    const comments = await Comment.find({ post: req.params.postId })
+      .populate("author", "firstName lastName email")
+      .populate("post", "title content");
+    console.log(comments);
     res.json(comments);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getCommentById = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const comment = await Comment.findById(req.params.id).populate(
+      "author",
+      "firstName lastName email"
+    );
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+    res.json(comment);
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
@@ -26,6 +45,7 @@ export const getCommentsByPost = async (req, res) => {
 
 export const updateComment = async (req, res) => {
   const { content } = req.body;
+  console.log(content);
 
   try {
     const comment = await Comment.findById(req.params.id);
@@ -33,7 +53,9 @@ export const updateComment = async (req, res) => {
       return res.status(404).json({ message: "Comment not found" });
     }
 
-    if (comment.author.toString() !== req.user.userId) {
+    console.log(comment.author.toString());
+    console.log(req.user._id);
+    if (comment.author.toString() !== req.user._id.toString()) {
       return res.status(403).json({ message: "Not authorized" });
     }
 
@@ -48,16 +70,18 @@ export const updateComment = async (req, res) => {
 
 export const deleteComment = async (req, res) => {
   try {
+    console.log(req.params.id);
     const comment = await Comment.findById(req.params.id);
     if (!comment) {
       return res.status(404).json({ message: "Comment not found" });
     }
+    console.log(comment);
 
-    if (comment.author.toString() !== req.user.userId) {
+    if (comment.author.toString() !== req.user._id.toString()) {
       return res.status(403).json({ message: "Not authorized" });
     }
 
-    await comment.remove();
+    await comment.deleteOne();
     res.json({ message: "Comment deleted" });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
